0a76daf11f54af074cb4e1c52d317c47d2ea71cd
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-05-28 08:59:14 +0900

    [Feat] Singleton Pattern
    싱글톤 제네릭 패턴 추가

---
+++ Assets/Scripts/DesignPatterns/Singleton.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace DesignPattern
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T m_instance;
+        public static T Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    m_instance = FindObjectOfType<T>();
+                    DontDestroyOnLoad(m_instance);
+                }
+                return m_instance;
+            }
+        }
+
+        protected void SingletonInit()
+        {
+            if (m_instance != null && m_instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                m_instance = this as T;
+                DontDestroyOnLoad(m_instance);
+            }
+        }
+    }
+}
