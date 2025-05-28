d531410c1ab893e1024a2270cc4f01ff08592ee2
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-05-28 09:32:25 +0900

    [Feat] StateMachine
    상태패턴을 위한 StateMachine

---
+++ Assets/Scripts/StatePattern/StateMachine.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    public Dictionary<EState, BaseState> StateDic;
+    public BaseState CurState;
+
+    public StateMachine()
+    {
+        StateDic = new Dictionary<EState, BaseState>();
+    }
+
+    public void ChangeState(BaseState changedState)
+    {
+        if (CurState == changedState) return;
+
+        CurState.Exit();
+        CurState = changedState;
+        CurState.Enter();
+    }
+
+    public void FixedUpdate()
+    {
+        if(CurState.HasPhysics) CurState.FixedUpdate();
+    }
+}
