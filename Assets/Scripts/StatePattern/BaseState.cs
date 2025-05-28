cea8335a8cb2b4bce74ed1c6518f617bfc03c2dc
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-05-28 09:27:38 +0900

    [Feat] BaseState
    상태패턴을 위한 BaseState 추상 클래스

---
+++ Assets/Scripts/StatePattern/BaseState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseState
+{
+    public bool HasPhysics;
+
+    public abstract void Enter();
+
+    public abstract void Update();
+
+    public virtual void FixedUpdate() { }
+
+    public abstract void Exit();
+}
+
+public enum EState
+{
+    Idle, Walk, Jump, meleeAttack, rangedAttack
+}
