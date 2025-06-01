8123f4da80ed170184248b2830aff217ebdbbc04
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-06-01 10:42:08 +0900

    [Feat] Stage2 Map, PortalTrigger
    스테이지2 맵을 만들고, 전환할 씬을 선택할 수 있는 포탈 트리거 기능을 구현함

--- Assets/Scripts/Managers/SceneChanger.cs
+++ Assets/Scripts/Managers/SceneChanger.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DesignPattern;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[Serializable]
 public enum SceneName 
 { 
     TitleScene,
     Stage1Scene,
     Stage2Scene,
+    Stage3_1Scene,
+    Stage3_2Scene,
 }
 
 public class SceneChanger : Singleton<SceneChanger>
--- Assets/Scripts/MapTrigger/PortalTrigger.cs
+++ Assets/Scripts/MapTrigger/PortalTrigger.cs
@@ -1,12 +1,16 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PortalTrigger : MonoBehaviour
 {
+    [SerializeField] private SceneName m_sceneName;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-         {
-            SceneChanger.Instance.SceneChange(SceneName.Stage2Scene);
+        {
+            SceneChanger.Instance.SceneChange(m_sceneName);
         }
     }
 }
